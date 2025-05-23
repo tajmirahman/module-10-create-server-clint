95e044f1d57dc66018bc0b662db08b1ffb9b1bbe
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-23 20:41:51 +0600

    clint side is starting

---
+++ first-express-file-clint-side/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [user,setUser]=useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res=>res.json())
+    .then(data=>setUser(data))
+  },[])
+
+  return (
+    <>
+      
+      <h1>Users Managment</h1>
+      <h2>Total Users- {user.length}</h2>
+      
+    </>
+  )
+}
+
+export default App
