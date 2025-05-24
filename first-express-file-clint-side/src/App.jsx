40bd6522a15b9e36bec0bbdbc164e5f20e5990a9
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-24 13:34:04 +0600

    user do not add from clint side to server side

--- first-express-file-clint-side/src/App.jsx
+++ first-express-file-clint-side/src/App.jsx
@@ -22,7 +22,26 @@ function App() {
     const name=form.name.value;
     const email=form.email.value;
 
-    console.log(name,email)
+    const user={
+      name,email
+    }
+
+    console.log(user)
+
+    fetch('http://localhost:5000/users',{
+      method: 'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=> {
+      console.log(data)
+      const newUsers =[...users,data];
+      setUser(newUsers);
+      form.reset();
+    })
 
   }
 
